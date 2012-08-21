88ffb69f8a0c03001adf96447686515b44fac32c
Tomek Miodek <tomek.miodek@gmail.com>
2012-08-21 22:15:51 +0200

    - added 'media' and 'static' resource dirs - added local_settings import - added base template

--- xo/settings.py
+++ xo/settings.py
@@ -1,6 +1,14 @@
+import os
+
 # Django settings for xo project.
 
-DEBUG = True
+# Set PROJECT_DIR to current directory.
+PROJECT_DIR = os.path.dirname(__file__)
+# Meida and static assets directory.
+PUBLIC_DIR = os.path.join(PROJECT_DIR, 'public')
+
+# Disable debug by default.
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -45,18 +53,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PUBLIC_DIR, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PUBLIC_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -76,9 +84,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '#^p$+xf$wb6@()gjxlewlcth1_3eyj73=4y_zy!bqg99v2r2if'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -106,6 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -149,3 +155,12 @@ LOGGING = {
         },
     }
 }
+
+# Load local settings configuration.
+try:
+    LOCAL_SETTINGS
+except NameError:
+    try:
+        from local_settings import *
+    except ImportError:
+        pass
--- xo/urls.py
+++ xo/urls.py
@@ -4,7 +4,9 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
+
     # Examples:
     # url(r'^$', 'xo.views.home', name='home'),
     # url(r'^xo/', include('xo.foo.urls')),
