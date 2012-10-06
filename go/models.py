945e0ea83e39bfffcaf592b78a252dafe9338938
Tomek Miodek <tomek.miodek@gmail.com>
2012-10-06 17:15:37 +0200

    - added 'size' property to Board class - added Board class unit tests

--- go/models.py
+++ go/models.py
@@ -8,9 +8,27 @@ from common.models import Game
 class Board(models.Model):
     game = models.OneToOneField(Game, primary_key=True)
 
+    sizes = (
+        (u'9x9',   u'Small'),
+        (u'13x13', u'Regular'),
+        (u'19x19', u'Large'),
+    )
+    size = models.CharField(max_length=5, choices=sizes, default='13x13')
+
+    def __init__(self, *args, **kwargs):
+        # Run base constructor
+        super(Board, self).__init__(*args, **kwargs)
+
+        # Set rows and cols values
+        self.rows,self.cols = map(
+            lambda(c) : int(c),
+            self.size.split('x')
+        )
+
 # Cell
 class Cell(models.Model):
     board = models.ForeignKey(Board)
+
 
 # Stone
 class Stone(models.Model):
--- go/tests.py
+++ go/tests.py
@@ -6,11 +6,49 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from models import Board
+from common.models import Game
 
+class BoardTest(TestCase):
+    # Load fixtures
+    fixtures = ['auth', 'common']
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_create_board(self):
+        print("")
+
+        # CASE
+        print("Create empty Board instance")
+        board = Board()
+        board.save()
+        self.assertIsInstance(board, Board)
+        self.assertEqual(board.size, "13x13")
+        self.assertEqual(board.rows, 13)
+        self.assertEqual(board.cols, 13)
+
+        # CASE
+        print("Create Board instance of defined size")
+        board = Board(size="9x9")
+        board.save()
+        self.assertEqual(board.size, "9x9")
+        self.assertEqual(board.rows, 9)
+        self.assertEqual(board.cols, 9)
+
+    def test_assing_board_to_game(self):
+        print("")
+
+        # CASE
+        print("Create Board instance and assing to a Game")
+        game = Game.objects.get(id=1)
+        board = Board(game_id=game.id)
+        board.save()
+        self.assertIsInstance(board, Board)
+
+        # CASE
+        print("Get Game by Board id")
+        self.assertIsInstance(board.game, Game)
+        self.assertTrue(board.game.id, game.id)
+
+        # CASE
+        print("Get Board by Game id")
+        self.assertIsInstance(game.board, Board)
+        self.assertTrue(game.board.pk, board.pk)
