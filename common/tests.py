ccbf44e18efbe6564dd1c6bea727da68c44b14e9
Tomek Miodek <tomek.miodek@gmail.com>
2012-10-06 17:14:18 +0200

    - updated Game model unit tests - added Game fixtures

--- common/tests.py
+++ common/tests.py
@@ -10,40 +10,55 @@ from django.contrib.auth.models import User
 from models import Game
 
 class GameTest(TestCase):
-    # Load User fixtures
+    # Load fixtures
     fixtures = ['auth']
 
     def test_create_game(self):
-        print()
+        print("")
 
-        print("Create Game instance")
+        # CASE
+        print("Create empty Game instance")
         game = Game()
-        self.assertTrue(isinstance(game, Game))
+        game.save()
+        self.assertIsInstance(game, Game)
 
     def test_assing_players(self):
-        print()
+        print("")
 
-        print("Create empty Game instance")
+        # CASE
+        print("Create Game instance")
         game = Game()
         game.save()
         self.assertEqual(len(game.users.filter()), 0)
 
-        print("Add two users to existing Game instance")
+        # CASE
+        print("Add two Users to existing Game instance")
         user1 = User.objects.get(id=1)
         user2 = User.objects.get(id=2)
         game.users.add(user1, user2)
         game.save()
         self.assertEqual(len(game.users.filter()), 2)
 
-        print("Get Game user by id")
-        self.assertEqual(len(game.users.filter(id=1)), 1)
+        # CASE
+        for i in (1,2):
+            print("Get Game User by id")
+            self.assertEqual(len(game.users.filter(id=i)), 1)
+            print("Check User class")
+            self.assertIsInstance(game.users.get(id=i), User)
 
-        print("Check User class")
-        self.assertTrue(isinstance(game.users.get(id=1), User))
-
+        # CASE
         for user in game.users.filter():
             print("Check Game instance by User %s" % user)
             game = user.game_set.get(id=1)
-            self.assertTrue(isinstance(game, Game))
+            self.assertIsInstance(game, Game)
+            print("Get all Game instances for User %s" % user)
+            games = user.game_set.filter()
+            self.assertEqual(len(games), 1)
 
+        # CASE
+        print("Remove Users from Game")
+        game.users.remove(user1, user2)
+        self.assertEqual(len(game.users.filter()), 0)
+        for user in (user1, user):
+            self.assertEqual(len(user.game_set.filter()), 0)
 
